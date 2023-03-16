24c9f330bb1cee81d75b380738309bc76b267644
dntrindade <dno.trindade@gmail.com>
2023-03-16 20:41:14 -0300

    voltando para a primeira mudanca

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação esta se conectando ao banco de dados")
-console.log("O paulo fez esse commit sem você saber!")
-console.log("Enviando para o GitHub")
