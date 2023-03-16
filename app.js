757bbad79bea8d8d6571b423687ffcc7b1576c57
dntrindade <dno.trindade@gmail.com>
2023-03-16 20:29:15 -0300

    adicionando um dois console log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação esta se conectando ao banco de dados")
+console.log("O paulo fez esse commit sem você saber!")
+console.log("Enviando para o GitHub")
